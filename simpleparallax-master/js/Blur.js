8230bfc8dfdf86823a3819bb192152e5af0b399f
KalliHooper <krhooper@mtu.edu>
2018-06-30 19:04:15 -0400

    a

---
+++ simpleparallax-master/js/Blur.js
@@ -0,0 +1,34 @@
+const inputs=document.querySelectorAll('.controls input');
+
+    function handleUpdate(){
+      console.log(this.value);
+      const suffix=this.dataset.sizing || '';
+      console.log(suffix);
+      document.documentElement.style.setProperty(`--${this.name}`,this.value+suffix);
+
+    }
+    inputs.forEach(input=>input.addEventListener('change', handleUpdate));
+    inputs.forEach(input=>input.addEventListener('mousemove', handleUpdate));
+
+     var images = [
+    'img/me1.PNG', 
+    'img/me2.PNG', 
+    'img/me3.PNG'
+];
+var num = 0;
+function next() {
+    var slider = document.getElementById('slider');
+    num++;
+    if(num >= images.length) {
+        num = 0;
+    }
+    slider.src = images[num];
+}
+function prev() {
+    var slider = document.getElementById('slider');
+    num--;
+    if(num < 0) {
+        num = images.length-1;
+    }
+    slider.src = images[num];
+}
